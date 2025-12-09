a6f8b4aab1b09b7c41c6b64b69c9aef3a22356d0
Timothy Ongaba <dextertim5@gmail.com>
2025-12-09 10:20:34 +0000

    Add files via upload

---
+++ 2025.12.8ChristmasBioinfoSOng.r
@@ -0,0 +1,73 @@
+#Christmas bioinformatics song
+#To the tune of "Jingle Bells"
+#Verse 1
+'''
+Dashing through the code,
+In a bioinformatics way,
+Analyzing genes and more,
+With algorithms that slay.
+Data sets so vast,
+Processing them with speed,
+Oh, what fun it is to code,
+In this scientific creed!
+  
+#Chorus
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way, hey!
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way!
+  
+#Verse 2
+Sequencing the DNA,
+Aligning reads with care,
+Finding patterns in the strands,
+With bioinformatics flair.
+Statistical models run,
+Predicting functions true,
+Oh, what fun it is to work,
+In this field so new!
+  
+#Chorus
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way, hey!
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way!
+  
+#Bridge
+From genomes to proteomes,
+We analyze it all,
+With scripts and tools we hone,
+Answering biology's call.
+
+#Chorus
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way, hey!
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way!
+
+#Outro
+So here's to all the coders,
+In bioinformatics cheer,
+Merry Christmas and happy coding,
+Throughout the coming year!
+  
+Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to code,
+In a bioinformatics way!'''
+  
+
+  
+  
